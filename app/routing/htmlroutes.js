e935227600efa1a00ed7aecbf69de784eb72ac0e
Andrew Reyes <Andyrc6@gmail.com>
2017-12-20 12:54:07 -0700

    fix?

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,18 +1,14 @@
 var connection = require('../data/friends.js');
-
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+  });
 
 module.exports = function(app){
     app.get("/", function(request, response){
-        connection.connect(function(err) {
-            if (err) throw err;
-            console.log("connected as id " + connection.threadId);
-            connection.query("select * from friends", function(err, data){
-                console.log(data);
-                connection.end();
-                response.render('index');
-            })
-          });
-        
-        
+        connection.query("select * from friends", function(err, data){
+            console.log(data);
+        })
+        response.render('index');
     })
 }
